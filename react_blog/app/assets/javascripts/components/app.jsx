b0b386d334b6c21524a079aa55b4234f8e8e16f7
Paul Duca <paul.r.duca@gmail.com>
2017-01-15 09:52:25 -0500

    working form

--- react_blog/app/assets/javascripts/components/app.jsx
+++ react_blog/app/assets/javascripts/components/app.jsx
@@ -1,10 +1,26 @@
 class App extends React.Component {
-  render(){
-    return(
-      <div className="main">
-        <h1>Hello World</h1>
-        <p>This is my blog! </p>
-      </div>
-    )
+
+  constructor(props){
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit() {
+    let name = this.refs.name.value;
+    let password = this.refs.password.value;
+    console.log('here')
+    alert('name: ' + name + ' password: ' + password)
+  }
+
+
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input ref='name' placeholder='stuff goes here'/>
+        <input ref='password' input='password' />
+        <input type="submit" value="Submit" />
+      </form>
+    );
   }
 }
